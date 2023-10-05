6e0b273f329207a4ca43b70b41bcd5f5304d0b6e
victor <devictoryghor@gmail.com>
2023-10-05 00:04:30 -0300

    update the initial files

---
+++ app/src/main/java/com/simplify/simplify/Presentation1.kt
@@ -0,0 +1,22 @@
+package com.simplify.simplify
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Presentation1.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Presentation1 : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
