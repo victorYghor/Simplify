26df5ebd9945553894a1e5c0c84507fdd3e31fce
victor <devictoryghor@gmail.com>
2023-10-11 11:52:40 -0300

    changin app name

---
+++ app/src/main/java/com/symplify/symplify/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.simplify.symplify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
