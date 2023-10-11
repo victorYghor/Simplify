f773679013bdeab0399a53fd46542f25bcd03585
victor <devictoryghor@gmail.com>
2023-10-11 17:46:50 -0300

    putting the jetpack compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,14 +33,32 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
 }
 
 dependencies {
+    // what is composebo
+    val composeBom = platform("androidx.compose:compose-bom:2023.10.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
 
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui")
+
+    // material icons
+    implementation("androidx.compose.material:material-icons-extended")
+    // integration with activities
+    implementation("androidx.activity:activity-compose:1.8.0")
+    // integration with viewModels
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
