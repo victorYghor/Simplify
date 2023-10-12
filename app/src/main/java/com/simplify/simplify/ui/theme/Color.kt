6e19759e1a8388772b212bb245f1726a9793734f
victor <devictoryghor@gmail.com>
2023-10-12 19:48:41 -0300

    changing the app colors name

--- app/src/main/java/com/simplify/simplify/ui/theme/Color.kt
+++ app/src/main/java/com/simplify/simplify/ui/theme/Color.kt
@@ -3,5 +3,5 @@ package com.simplify.simplify.ui.theme
 import androidx.compose.ui.graphics.Color
 
 // this is the colors of the app that
-val Blue800 = Color(0xFF872C5)
-val Black = Color(0xFF00000)
+val Blue800 = Color(0xFF0872C5)
+val Black = Color(0xFF0E0E0E)
