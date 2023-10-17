2aa74852c35badb017fa4c186618699756e1ad03
victor <devictoryghor@gmail.com>
2023-10-17 00:57:08 -0300

    enter screen complete

--- app/src/main/java/com/simplify/simplify/ui/components/Buttons.kt
+++ app/src/main/java/com/simplify/simplify/ui/components/Buttons.kt
@@ -1,17 +1,17 @@
 package com.simplify.simplify.ui.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.ButtonDefaults.filledTonalButtonColors
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,8 +20,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.simplify.simplify.R
-import com.simplify.simplify.ui.theme.Black
 import com.simplify.simplify.ui.theme.Blue800
+import com.simplify.simplify.ui.theme.LinkedinBlue
 import com.simplify.simplify.ui.theme.simplifyTypography
 
 @Composable
@@ -41,31 +41,34 @@ fun SymplifyButtons(
 
 @Composable
 fun ContinueWithButtons(
-    textRes: Int,
+    text: String,
     onClick: () -> Unit,
     modifier: Modifier,
     icon: Int,
     textColor: Color,
-    backgroundColor: Color
+    backgroundColor: Color,
+    border: BorderStroke?
 ) {
     Button(
         onClick = { onClick() },
         modifier = modifier.fillMaxWidth(0.9f),
-        colors = ButtonColors(backgroundColor = backgroundColor)
+        colors = buttonColors(containerColor = backgroundColor, contentColor = textColor),
+        border = border
     ) {
         Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
+            horizontalArrangement = Arrangement.Start,
             verticalAlignment = Alignment.CenterVertically,
-            modifier = modifier.padding(16.dp).fillMaxWidth()
+            modifier = modifier.fillMaxWidth()
         ) {
             Image(
                 painterResource(icon),
                 contentDescription = null,
-                modifier = Modifier.size(64.dp)
+                modifier = Modifier.size(48.dp)
             )
-            SimplifyText(
-                textRes,
-                simplifyTypography.titleSmall,
+            Spacer(modifier = Modifier.size(16.dp))
+            Text(
+                text,
+                style = simplifyTypography.bodySmall,
                 color = textColor
             )
         }
@@ -75,10 +78,13 @@ fun ContinueWithButtons(
 @Composable
 fun SymplifyButtonPreview() {
     ContinueWithButtons(
-        R.string.enter,
+        "Enter",
         modifier = Modifier,
         onClick = {},
-        icon = R.drawable.linkedin
+        icon = R.drawable.linkedin,
+        backgroundColor = LinkedinBlue,
+        textColor = Color.White,
+        border = null
     )
 }
 
--- app/src/main/java/com/simplify/simplify/ui/login/LoginScreen.kt
+++ app/src/main/java/com/simplify/simplify/ui/login/LoginScreen.kt
@@ -4,31 +4,26 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.simplify.simplify.R
 import com.simplify.simplify.ui.components.ContinueWithButtons
-import com.simplify.simplify.ui.components.SymplifyButtons
 import com.simplify.simplify.ui.components.SymplifyLogo
+import com.simplify.simplify.ui.theme.Gray0
+import com.simplify.simplify.ui.theme.Gray50
+import com.simplify.simplify.ui.theme.Gray900
+import com.simplify.simplify.ui.theme.LinkedinBlue
 
 @Composable
 fun LoginScreen() {
     Column(verticalArrangement = Arrangement.SpaceAround, horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
         SymplifyLogo()
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            ContinueWithButtons(
-               textRes = R.string.linkedin,
-                onClick = { },
-                icon = R.drawable.linkedin,
-                modifier = Modifier
-            )
-        }
+
     }
 
 }
